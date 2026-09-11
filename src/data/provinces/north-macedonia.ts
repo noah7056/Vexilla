0570557571b02467d83c0d538d518f77e155a6e7
noah7056 <jellytuber946@gmail.com>
2026-09-11 22:20:39 +0200

    \Set North Macedonia province flags status to official"

--- src/data/provinces/north-macedonia.ts
+++ src/data/provinces/north-macedonia.ts
@@ -8,566 +8,566 @@ export const NORTH_MACEDONIA_PROVINCES = createProvinceFlags('North Macedonia', 
     code: 'mk-aer',
     image: 'Flag_of_Aerodrom_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Aračinovo',
     code: 'mk-ara',
     image: 'Flag_of_Aracinovo_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Berovo',
     code: 'mk-ber',
     image: 'Flag_of_Berovo_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Bitola',
     code: 'mk-bit',
     image: 'Flag_of_Bitola_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Bogdanci',
     code: 'mk-bgd',
     image: 'Flag_of_Bogdanci_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Bogovinje',
     code: 'mk-bog',
     image: 'Flag_of_Bogovinje_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Bosilovo',
     code: 'mk-bos',
     image: 'Flag_of_the_Bosilovo_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Brvenica',
     code: 'mk-brv',
     image: 'Flag_of_Brvenica_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Butel',
     code: 'mk-but',
     image: 'Flag_of_Butel_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Čair',
     code: 'mk-cai',
     image: 'Flag_of_Čair_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Čaška',
     code: 'mk-cas',
     image: 'Flag_of_Čaška_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Centar',
     code: 'mk-cen',
     image: 'Flag_of_Centar_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Centar Župa',
     code: 'mk-cez',
     image: 'Flag_of_Centar_Župa_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Češinovo-Obleševo',
     code: 'mk-ceo',
     image: 'Flag_of_Cheshinovo-Obleshevo.jpg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Čučer-Sandevo',
     code: 'mk-cus',
     image: 'Знаме_на_Општина_Чучер-Сандево.png',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Debar',
     code: 'mk-deb',
     image: 'Flag_of_Dibër_Municipality.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Debarca',
     code: 'mk-dbc',
     image: 'Flag_of_Debarca_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Delčevo',
     code: 'mk-del',
     image: 'Flag_of_Delčevo_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Demir Hisar',
     code: 'mk-deh',
     image: 'Zname-opstina-Demir-Hisar.jpg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Demir Kapija',
     code: 'mk-dek',
     image: 'Flag_of_Demir_Kapija_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Dojran',
     code: 'mk-doj',
     image: 'Flag_of_Dojran_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Dolneni',
     code: 'mk-dol',
     image: 'Flag_of_Dolneni_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Gazi Baba',
     code: 'mk-gab',
     image: 'Flag_of_Gazi_Baba_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Gevgelija',
     code: 'mk-gev',
     image: 'Flag_of_Gevgelija_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Gjorče Petrov',
     code: 'mk-gjp',
     image: 'Flag_of_Ǵorče_Petrov_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Gostivar',
     code: 'mk-gos',
     image: 'Flag_of_Gostivar_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Gradsko',
     code: 'mk-gra',
     image: 'Mk-grad.gif',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Ilinden',
     code: 'mk-ili',
     image: 'Flag_of_Ilinden.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Jegunovce',
     code: 'mk-jeg',
     image: 'Flag_of_Jegunovce_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Karbinci',
     code: 'mk-krb',
     image: 'Flag_of_Karbinci_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Karpoš',
     code: 'mk-kps',
     image: 'Flag_of_Karpoš_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Kavadarci',
     code: 'mk-kav',
     image: 'Flag_of_Kavadarci_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Kičevo',
     code: 'mk-kic',
     image: 'Flag_of_Kičevo_Municipality.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Kisela Voda',
     code: 'mk-kiv',
     image: 'Flag_of_Kisela_Voda_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Konče',
     code: 'mk-kon',
     image: 'Flag_of_Konče_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Kocani',
     code: 'mk-kcn',
     image: 'Flag_of_Kocani_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Kratovo',
     code: 'mk-kra',
     image: 'Flag_of_Kratovo_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Kriva Palanka',
     code: 'mk-krp',
     image: 'Flag_of_Kriva_Palanka_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Krivogaštani',
     code: 'mk-kri',
     image: 'Flag_of_Krivogaštani_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Kruševo',
     code: 'mk-kru',
     image: 'Flag_of_Kruševo_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Kumanovo',
     code: 'mk-kum',
     image: 'Flag_of_Kumanovo_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Lipkovo',
     code: 'mk-lip',
     image: 'Flag_of_Lipkovo_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Lozovo',
     code: 'mk-loz',
     image: 'Flag_of_Lozovo_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Makedonska Kamenica',
     code: 'mk-mak',
     image: 'Flag_of_Makedonska_Kamenica_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Makedonski Brod',
     code: 'mk-mab',
     image: 'Flag_of_Makedonski_Brod_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Mavrovo and Rostuša',
     code: 'mk-mar',
     image: 'Flag_of_Mavrovo_and_Rostuša_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Mogila',
     code: 'mk-mog',
     image: 'Flag_of_Mogila_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Negotino',
     code: 'mk-neg',
     image: 'Flag_of_Negotino_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Novaci',
     code: 'mk-nov',
     image: 'Flag_of_Novaci_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Novo Selo',
     code: 'mk-nos',
     image: 'Flag_of_Novo_Selo.gif',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Ohrid',
     code: 'mk-ohr',
     image: 'Mk-ohri.png',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Pehčevo',
     code: 'mk-peh',
     image: 'Flag_of_Pehčevo_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Petrovec',
     code: 'mk-pet',
     image: 'Flag_of_Petrovec_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Plasnica',
     code: 'mk-pla',
     image: 'Flag_of_Plasnica_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Prilep',
     code: 'mk-pri',
     image: 'Flag_of_Prilep_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Probištip',
     code: 'mk-pro',
     image: 'Probistip-zname.png',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Radoviš',
     code: 'mk-rad',
     image: 'Flag_of_Radoviš_Municipality.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Rankovce',
     code: 'mk-ran',
     image: 'Flag_of_Rankovce_Municipality.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Resen',
     code: 'mk-res',
     image: 'Flag_of_Resen_Municipality.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Rosoman',
     code: 'mk-ros',
     image: 'Flag_of_Rosoman_Municipality.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Saraj',
     code: 'mk-sar',
     image: 'Flag_of_Saraj_Municipality.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Skopje',
     code: 'mk-sko',
     image: 'Flag_of_Skopje,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Sopište',
     code: 'mk-sop',
     image: 'Flag_of_Sopište_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Staro Nagoričane',
     code: 'mk-stn',
     image: 'Flag_of_Staro_Nagoričane,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Štip',
     code: 'mk-sti',
     image: 'Flag_of_Štip_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Struga',
     code: 'mk-str',
     image: 'Flag_of_Struga_Municipality.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Strumica',
     code: 'mk-stm',
     image: 'Flag_of_Strumica_Municipality.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Studeničani',
     code: 'mk-stu',
     image: 'Flag_of_Studeničani_municipality.png',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Šuto Orizari',
     code: 'mk-suo',
     image: 'Flag_of_Šuto_Orizari_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Sveti Nikole',
     code: 'mk-svn',
     image: 'Flag_of_Sveti_Nikole,_North_Macedonia.jpg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Tearce',
     code: 'mk-tea',
     image: 'Flag_of_Tearce_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Tetovo',
     code: 'mk-tet',
     image: 'Flag_of_Tetovo_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Valandovo',
     code: 'mk-val',
     image: 'Flag_of_Valandovo_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Vasilevo',
     code: 'mk-vas',
     image: 'Flag_of_Vasilevo_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Vevčani',
     code: 'mk-vev',
     image: 'Flag_of_Vevčani_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Veles',
     code: 'mk-vel',
     image: 'Flag_of_Veles_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Vinica',
     code: 'mk-vin',
     image: 'Flag_of_Vinica_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Vrapčište',
     code: 'mk-vra',
     image: 'Flag_of_Vrapčište_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Zelenikovo',
     code: 'mk-zlk',
     image: 'Flag_of_Zelenikovo_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Želino',
     code: 'mk-zln',
     image: 'Flag_of_Želino_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   },
   {
     name: 'Zrnovci',
     code: 'mk-zrn',
     image: 'Flag_of_Zrnovci_Municipality,_North_Macedonia.svg',
     tags: [],
-    status: ""
+    status: "official"
   }
 ], 'Europe');
